29002ca068e63ee833500b5eb464d7c7edad77ec
akiko.vandal@gmail.com <akiko.vandal@gmail.com>
2022-11-24 11:31:53 +0100

    created get tip by id router

--- routers/tipsRouter.js
+++ routers/tipsRouter.js
@@ -8,5 +8,18 @@ router.get("/", async (req, res) => {
     res.status(200).send({ message: "ok", tips });
 });
 
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+        return res.status(404).send({ message: "Tip id is not a number" });
+    }
+    const tipById = await IngredientDescription.findByPk(id);
+
+    if (tipById === null) {
+        return res.status(404).send({ message: "Tip not found" });
+    }
+    res.status(200).send({ message: "ok", tipById });
+});
 
 module.exports = router;
